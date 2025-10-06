7b1930133d2990f5c7028c61833ad2e77c5ccda4
arquilly <163546496+arquilly@users.noreply.github.com>
2025-10-06 21:08:56 +0300

    Update task1.py

--- homework5/task1.py
+++ homework5/task1.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 for i in range(10):
     symbols = '0123456789abcdefghijklmnopqrstuvwxyz'
-    filename=""
+    filename="homework5/"
     for j in range(8):
         filename += random.choice(symbols)
     filename+=".txt"
